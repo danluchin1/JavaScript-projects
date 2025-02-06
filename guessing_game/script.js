2e780114a4bd68319f3cfaca59b6992c16b87d45
Luchin Dan <144890437+danluchin1@users.noreply.github.com>
2025-02-06 14:56:20 +0200

    Update script.js

--- guessing_game/script.js
+++ guessing_game/script.js
@@ -1,26 +1,25 @@
-'use strict';
 const minNum = 1;
 const maxNum = 100;
 const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 
-let attemps = 0;
-let guess;
-let running = true;
+let attempts = 0;
 
-while(running){
-  guess = +prompt(`Guess a number between ${minNum} and ${maxNum}.`);
+function guessNumber() {
+  const textBox = document.getElementById("textBox");
+  const result = document.getElementById("result");
 
-  if(isNaN(guess) || guess < 1 || guess > 100){
-    alert("Please enter a valid number!");
-  }else{
-    attemps++;
-    if(guess < answer){
-      alert("TOO LOW! TRY AGAIN!");
-    }else if(guess > answer){
-      alert("TOO HIGH! TRY AGAIN!");
-    }else{
-      alert(`CORRECT! It took you ${attemps} attemps!`);
-      running = false;
+  let userGuess = parseInt(textBox.value);
+
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+    result.textContent = `Please enter a valid number between ${minNum} and ${maxNum}!`;
+  } else {
+    attempts++;
+    if (userGuess < answer) {
+      result.textContent = "TOO LOW! TRY AGAIN!";
+    } else if (userGuess > answer) {
+      result.textContent = "TOO HIGH! TRY AGAIN!";
+    } else {
+      result.textContent = `CORRECT! It took you ${attempts} attempts!`;
     }
   }
 }
